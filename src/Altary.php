1a0e0510ac698ab40641029a3d865c938d193742
nomura <masahiroview55@gmail.com>
2025-05-23 16:23:29 +0900

    ファイル追加

---
+++ src/Altary.php
@@ -0,0 +1,49 @@
+<?php
+namespace Altary;
+
+use Altary\Internal\Client;
+use Altary\Internal\ErrorHandler;
+use Altary\AltaryRegistry;
+
+class Altary
+{
+    public static function init(array $options = []): void {
+        static $initialized = false;
+        if ($initialized) return;
+        $initialized = true;
+
+        $apiKey = $options['api_key'] ?? null;
+        if (!$apiKey) {
+            throw new \InvalidArgumentException('Altary init: api_key is required');
+        }
+
+        $client = new Client($apiKey);
+
+        if (isset($options['user'])) {
+            $client->setUser($options['user']);
+        }
+
+        if (isset($options['log_levels'])) {
+            $client->setLogLevels($options['log_levels']);
+        }
+
+        $handler = new ErrorHandler($client);
+        $handler->register();
+
+        AltaryRegistry::setClient($client);
+    }
+
+    public static function handleException(\Throwable $e): void {
+        $client = AltaryRegistry::getClient();
+        if ($client) {
+            $client->send([
+                'type' => 'exception',
+                'message' => $e->getMessage(),
+                'file' => $e->getFile(),
+                'line' => $e->getLine(),
+                'trace' => $e->getTraceAsString(),
+                'file_content' => $client->getFileContentSafe($e->getFile())
+            ]);
+        }
+    }
+}
